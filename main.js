0bc40b9ed52abf8572242d1bb93c2da4b688e44d
CodeXMalinga <rdimalingadesilva@gmail.com>
2021-10-14 22:17:51 +0530

    Added JS Functionality

---
+++ main.js
@@ -0,0 +1,28 @@
+const slides = document.querySelectorAll('.slide');
+const dots = document.querySelectorAll('.dot');
+
+
+    function setActive(i){
+        slides.forEach(
+            function(slide){
+                slide.classList.remove('active');
+                slides[i].classList.add('active');
+            }
+        )
+
+        dots.forEach(
+            function(dot){
+                dot.classList.remove('active');
+                dots[i].classList.add('active');
+            }
+        )
+
+    }
+
+      for (let j = 0; j < dots.length; j++)
+        dots[j].addEventListener('click', function() {
+          setActive(j);
+        })
+
+
+   
